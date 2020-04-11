ee9cd82d3b6fab2dcdee62e1292c819e87520d9a
kkalkidan <fkalkidan30@gmail.com>
2020-04-11 10:35:11 +0300

    Removed irrelevant tests

--- test/intelligentAgent/agent_test.h
+++ test/intelligentAgent/agent_test.h
@@ -1,17 +1,3 @@
-#include <cxxtest/TestSuite.h>
-#include <world/world.hpp>
-#include <world/room.hpp>
-#include <util/converter.hpp>
-#include <agent/agent.hpp>
 
-
-class TestAgent: public CxxTest::TestSuite
-{
-   public:
-
-      void test_agent( void )
-      {
-        
-      }
 
       
--- test/util/converter_test.h
+++ test/util/converter_test.h
@@ -1,16 +1,2 @@
-#include <cxxtest/TestSuite.h>
-#include <world/world.hpp>
-#include <world/room.hpp>
-#include <util/converter.h>
-
-
-class TestConverter : public CxxTest::TestSuite
-{
-   public:
-
-      void test_converts( void )
-      {
-        
-      }
 
       
